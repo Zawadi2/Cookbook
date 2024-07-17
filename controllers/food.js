da5a483a6f866d690a16ab898fccaad17dd34d9a
Zawadi2 <zawadin95@gmail.comm>
2024-07-17 18:03:00 -0500

    Furst commit"

---
+++ controllers/food.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+
+
+// const User = require("../models/user.js");
+// router.get("/sign-up", (req, res) => {
+//     res.render("sign-up.ejs");
+//   });
+  
+module.exports = router;
---
+++ models/user.js
@@ -0,0 +1,14 @@
+const userSchema = new mongoose.Schema({
+    username: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    pantry:[foodSchema]
+  });
+  const User = mongoose.model("User", userSchema);
+  
+  module.exports = User;
